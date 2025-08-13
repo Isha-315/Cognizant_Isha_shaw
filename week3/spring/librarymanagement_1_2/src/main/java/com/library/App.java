8bd5d7301d0111f2597f35b06adedf06a2fc6dde
Isha-315 <ishashaw2004@gmail.com>
2025-08-13 21:26:18 +0530

    Add files via upload

---
+++ week3/spring/librarymanagement_1_2/src/main/java/com/library/App.java
@@ -0,0 +1,14 @@
+package com.library;
+import com.library.App;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ week3/spring/librarymanagement_1_2/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.library.repository;
+
+public class BookRepository {
+    
+
+    public String findBookById(String id) {
+        return "Sample Book with ID: " + id;
+    }
+}
+
